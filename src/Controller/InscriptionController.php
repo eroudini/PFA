76cb8df3b36f796838cdacdaf3225ea6e4b8748c
eroudini <eroudini.abdillah@yahoo.com>
2024-01-10 15:25:33 +0100

    :bulb:

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -24,7 +24,7 @@ class InscriptionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
             $user = new User();
-            $user->setMail($data["email"]);
+            $user->setMail($data["mail"]);
             $user->setPassword($data["password"]);
             $em->persist($user);
             $em->flush();
@@ -49,7 +49,6 @@ class InscriptionController extends AbstractController
         dd($user);
     }
 
-
     #[Route("/user/{id}/update", name: "user_update", methods: ["GET"], requirements: ["id" => "\d+"])]
     public function update()
     {
@@ -57,15 +56,7 @@ class InscriptionController extends AbstractController
             "message" => "Ici la page de crétion d'un utilisateur"
         ]);
     }
-
-    #[Route("/user/{id}/delete", name: "user_delete", methods: ["GET"], requirements: ["id" => "\d+"])]
-    public function delete()
-    {
-        return $this->render('user/delete.html.twig', [
-            "message" => "Ici la page de crétion d'un utilisateur"
-        ]);
-    }
-
+    
     #[Route("/user/{id}/edit", name: "user_edit",  methods: ["GET", "POST"], requirements: ["id" => "\d+"])]
     public function edit($id, Request $request, UserRepository $userRepository, EntityManagerInterface $em)
     {
@@ -77,10 +68,27 @@ class InscriptionController extends AbstractController
             $user = $form->getData();
             $em->flush();
         }
-
+        
         return $this->render("user/edit.html.twig", [
             "form" => $form->createView()
         ]);
         dd($form);
+    }
+    
+    #[Route("/user/{id}/delete", name: "user_delete", methods: ["GET"], requirements: ["id" => "\d+"])]
+    public function delete($id, Request $request, UserRepository $userRepository, EntityManagerInterface $em,): Response
+    {
+        $user = $userRepository->find((int)$id);
+
+        if ($request->query->get('method')) 
+        {
+            $em->remove($user);
+            $em->flush();
+            
+            // dd($id);
+        }
+        return $this->render('user/read.html.twig', [
+            "user"=>$user,
+        ]);
     }
 }
