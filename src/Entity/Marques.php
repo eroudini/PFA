32afe157f3525f1c10e8dbe9da73a8156cd4ad91
eroudini <eroudini.abdillah@yahoo.com>
2024-04-19 15:51:47 +0200

    nc

--- src/Entity/Marques.php
+++ src/Entity/Marques.php
@@ -18,12 +18,8 @@ class Marques
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
-    #[ORM\OneToMany(mappedBy: 'Marque', targetEntity: Cars::class)]
-    private Collection $descriptions;
-
     public function __construct()
     {
-        $this->descriptions = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -39,36 +35,6 @@ class Marques
     public function setNom(string $nom): static
     {
         $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Cars>
-     */
-    public function getDescriptions(): Collection
-    {
-        return $this->descriptions;
-    }
-
-    public function addDescription(Cars $description): static
-    {
-        if (!$this->descriptions->contains($description)) {
-            $this->descriptions->add($description);
-            $description->setMarque($this);
-        }
-
-        return $this;
-    }
-
-    public function removeDescription(Cars $description): static
-    {
-        if ($this->descriptions->removeElement($description)) {
-            // set the owning side to null (unless already changed)
-            if ($description->getMarque() === $this) {
-                $description->setMarque(null);
-            }
-        }
 
         return $this;
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,7 +17,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
+    #[ORM\Column(length: 180)]
     private ?string $nom = null;
 
     #[ORM\Column(length: 180, unique: true)]
