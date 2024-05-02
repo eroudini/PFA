bc411a41ae8deb357c98663e3538486161c7afe0
eroudini <eroudini.abdillah@yahoo.com>
2024-05-02 17:18:48 +0200

    accres restreint admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,9 +10,10 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    //#[IsGranted('ROLE_ADMIN')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
+        
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
